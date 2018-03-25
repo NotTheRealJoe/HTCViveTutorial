cc42b0106bebffc8e9d7144ff4b06b7c9a9964ad
nottherealjoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-03-24 23:36:55 -0500

    Added teleportation related code to LaserPointer script

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -8,6 +8,14 @@ public class LaserPointer : MonoBehaviour {
     private GameObject laser;
     private Transform laserTransform;
     private Vector3 hitPoint;
+    public Transform cameraRigTransform;
+    public GameObject teleportReticlePrefab;
+    private GameObject reticle;
+    private Transform teleportReticleTransform;
+    public Transform headTransform;
+    public Vector3 teleportReticleOffset;
+    public LayerMask teleportMask;
+    private bool shouldTeleport;
 
     private SteamVR_Controller.Device Controller {
         get {
@@ -34,12 +42,20 @@ public class LaserPointer : MonoBehaviour {
         if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad)) {
             RaycastHit hit;
 
-            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100)) {
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100, teleportMask)) {
                 hitPoint = hit.point;
                 ShowLaser(hit);
+                reticle.SetActive(true);
+                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                shouldTeleport = true;
             }
         } else {
             laser.SetActive(false);
+            reticle.SetActive(false);
+        }
+
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && shouldTeleport) {
+            Teleport();
         }
     }
 
@@ -53,5 +69,15 @@ public class LaserPointer : MonoBehaviour {
     void Start() {
         laser = Instantiate(laserPrefab);
         laserTransform = laser.transform;
+        reticle = Instantiate(teleportReticlePrefab);
+        teleportReticleTransform = reticle.transform;
+    }
+
+    private void Teleport() {
+        shouldTeleport = false;
+        reticle.SetActive(false);
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+        difference.y = 0;
+        cameraRigTransform.position = hitPoint + difference;
     }
 }
