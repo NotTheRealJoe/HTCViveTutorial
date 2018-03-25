a523417be5344602914cb8ccfa3fdd76829b4e04
nottherealjoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-03-24 22:15:09 -0500

    Updated steamvr plugin

--- Assets/Scripts/ViveControllerInputTest.cs
+++ Assets/Scripts/ViveControllerInputTest.cs
@@ -3,37 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ViveControllerInputTest : MonoBehaviour {
-    // 1
     private SteamVR_TrackedObject trackedObj;
-    // 2
+    
     private SteamVR_Controller.Device Controller {
-	get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	    get {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    private void Start() {
+        Debug.Log("Controller test script loaded.");
     }
 
     void Awake() {
-	trackedObj = GetComponent<SteamVR_TrackedObject>();
+	    trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
     
-    // Update is called once per frame
     void Update () {
-	if(Controller.GetAxis() != Vector2.zero) {
-	    Debug.Log(gameObject.name + Controller.GetAxis());
-	}
+	    if(Controller.GetAxis() != Vector2.zero) {
+	        Debug.Log(gameObject.name + Controller.GetAxis());
+	    }
 
-	if(Controller.GetHairTriggerDown()) {
-	    Debug.Log(gameObject.name + " Trigger Press");
-	}
+	    if(Controller.GetHairTriggerDown()) {
+	        Debug.Log(gameObject.name + " Trigger Press");
+	    }
 
-	if(Controller.GetHairTriggerUp()) {
-	    Debug.Log(gameObject.name + " Trigger Release");
-	}
+	    if(Controller.GetHairTriggerUp()) {
+	        Debug.Log(gameObject.name + " Trigger Release");
+	    }
 
-	if(Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip)) {
-	    Debug.Log(gameObject.name + " Grip Press");
-	}
+	    if(Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip)) {
+	        Debug.Log(gameObject.name + " Grip Press");
+	    }
 
-	if(Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip)) {
-	    Debug.Log("gameObject.name" + " Grip Release");
-	}
+	    if(Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip)) {
+	        Debug.Log("gameObject.name" + " Grip Release");
+	    }
     }
 }
